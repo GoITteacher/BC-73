b91dadb4ef5d8398fb9f264c49146c17e270c241
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2024-06-13 09:16:12 +0300

    update

--- js/02-comp.js
+++ js/02-comp.js
@@ -3,34 +3,34 @@
  *
  * - Оператори порівняння
  * - Оператори рівності
- * - Приведение типов операндов
+ * - Приведение типів операндів
  */
 
 console.log(5 > 4);
 
-console.log(10 >= "7");
+console.log(10 >= '7');
 
-console.log("2" > "12");
+console.log('2' > '12');
 
-console.log("2" < "12");
+console.log('2' < '12');
 
-console.log("4" == 4);
+console.log('4' == 4);
 
-console.log("6" === 6);
+console.log('6' === 6);
 
-console.log("false" === false);
+console.log('false' === false);
 
 console.log(1 == true);
 
 console.log(1 === true);
 
-console.log("0" == false);
+console.log('0' == false);
 
-console.log("0" === false);
+console.log('0' === false);
 
-console.log("Papaya" < "papaya");
+console.log('Papaya' < 'papaya');
 
-console.log("Papaya" === "papaya");
+console.log('Papaya' === 'papaya');
 
 console.log(undefined == null);
 
