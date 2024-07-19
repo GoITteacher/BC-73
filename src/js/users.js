9b199d543cecb9d2dff8c8e56f7e4d1ca01a26d1
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2024-07-19 09:18:22 +0300

    update

--- src/js/users.js
+++ src/js/users.js
@@ -1,48 +1,133 @@
-import { UsersAPI } from './modules/usersAPI';
+import {
+  getUsers,
+  createUser,
+  updateUser,
+  resetUser,
+  deleteUser,
+} from './modules/usersAPI.js';
 
-// ===================================================
+//!======================================================
 const refs = {
   userListElem: document.querySelector('.js-user-list'),
   createUserForm: document.querySelector('.js-create-form'),
   updateUserForm: document.querySelector('.js-update-form'),
   resetUserForm: document.querySelector('.js-reset-form'),
-  deleteUserForm: document.querySelector('.js-delete-form'),
 };
-// ============================================================
 
-const usersAPI = new UsersAPI();
+//!======================================================
 
-usersAPI
-  .getUsers()
-  .then(data => {
-    renderUsers(data.reverse());
-  })
-  .catch(err => {
-    console.log(err);
+document.addEventListener('DOMContentLoaded', onUserLoad);
+refs.createUserForm.addEventListener('submit', onUserCreate);
+refs.updateUserForm.addEventListener('submit', onUserUpdate);
+refs.resetUserForm.addEventListener('submit', onUserReset);
+refs.userListElem.addEventListener('click', onUserDelete);
+
+//!======================================================
+
+function onUserLoad() {
+  getUsers().then(users => {
+    const markup = usersTemplate(users);
+    refs.userListElem.innerHTML = markup;
+  });
+}
+
+//? ==================================
+
+function onUserCreate(e) {
+  e.preventDefault();
+
+  const formData = new FormData(e.target);
+
+  const user = Object.fromEntries(formData.entries());
+
+  createUser(user).then(result => {
+    const markup = userTemplate(result);
+    refs.userListElem.insertAdjacentHTML('afterbegin', markup);
   });
 
-// ==========================================================
+  e.target.reset();
+}
+//? ==================================
 
-function templateUser({ id, name, email, phone, img }) {
-  return `
-  <li class="card user-item" data-id="${id}">
+function onUserUpdate(e) {
+  e.preventDefault();
+
+  const formData = new FormData(e.target);
+
+  const userData = Object.fromEntries(formData.entries());
+  const user = parseUserParamsForUpdate(userData);
+
+  updateUser(user).then(result => {
+    const markup = userTemplate(result);
+
+    const userElem = document.querySelector(`li[data-id="${user.id}"]`);
+    userElem.insertAdjacentHTML('afterend', markup);
+
+    userElem.remove();
+  });
+
+  e.target.reset();
+}
+//? ==================================
+
+function onUserReset(e) {
+  e.preventDefault();
+
+  const formData = new FormData(e.target);
+
+  const user = Object.fromEntries(formData.entries());
+
+  resetUser(user).then(result => {
+    const markup = userTemplate(result);
+
+    const userElem = document.querySelector(`li[data-id="${user.id}"]`);
+    userElem.insertAdjacentHTML('afterend', markup);
+
+    userElem.remove();
+  });
+
+  e.target.reset();
+}
+//? ==================================
+
+function onUserDelete(e) {
+  if (e.target.nodeName !== 'BUTTON') return;
+  const liElem = e.target.closest('li');
+  const id = liElem.dataset.id;
+
+  deleteUser(id).then(() => {
+    liElem.remove();
+  });
+}
+
+//!======================================================
+function userTemplate({ id, name, img, email, phone }) {
+  return `<li class="card user-item" data-id="${id}">
   <img
     src="https://picsum.photos/1280/720?random=${id}"
     alt="#"
     class="user-avatar"
   />
   <h3 class="user-title">${name}</h3>
-  <p>Phone: ${email}</p>
-  <p>Email: ${phone}</p>
+  <p>Phone: ${phone}</p>
+  <p>Email: ${email}</p>
   <button class="btn button">DELETE</button>
 </li>`;
 }
 
-function templateUsers(users) {
-  return users.map(templateUser).join('');
+function usersTemplate(arr) {
+  return arr.map(userTemplate).join('\n');
 }
 
-function renderUsers(users) {
-  const markup = templateUsers(users);
-  refs.userListElem.innerHTML = markup;
+function parseUserParamsForUpdate(user) {
+  const result = {};
+
+  for (const [key, value] of Object.entries(user)) {
+    if (value) {
+      result[key] = value;
+    }
+  }
+
+  return result;
 }
+//!======================================================
