4a9c175f4151f93d113675f94a4e9b0e573b7a51
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2024-07-19 09:17:06 +0300

    update

--- src/js/modules/usersAPI.js
+++ src/js/modules/usersAPI.js
@@ -1,12 +1,90 @@
-export class UsersAPI {
-  constructor() {
-    this.BASE_URL = 'http://localhost:3000';
-    this.END_POINT = '/users';
-    this.API_KEY = '123123';
-  }
+const BASE_URL = 'http://localhost:3000';
 
-  getUsers() {
-    const url = this.BASE_URL + this.END_POINT;
-    return fetch(url).then(res => res.json());
-  }
+export function getUsers() {
+  const END_POINT = '/users';
+  const url = BASE_URL + END_POINT;
+
+  return fetch(url).then(res => {
+    if (res.ok) {
+      return res.json();
+    } else {
+      throw new Error('Server Error');
+    }
+  });
+}
+export function createUser(user) {
+  const END_POINT = '/users';
+  const url = BASE_URL + END_POINT;
+
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(user),
+  };
+
+  return fetch(url, options).then(res => {
+    if (res.ok) {
+      return res.json();
+    } else {
+      throw new Error('Server Error');
+    }
+  });
+}
+export function updateUser({ id, ...user }) {
+  const END_POINT = `/users/${id}`;
+  const url = BASE_URL + END_POINT;
+
+  const options = {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(user),
+  };
+
+  return fetch(url, options).then(res => {
+    if (res.ok) {
+      return res.json();
+    } else {
+      throw new Error('Server Error');
+    }
+  });
+}
+export function resetUser({ id, ...user }) {
+  const END_POINT = `/users/${id}`;
+  const url = BASE_URL + END_POINT;
+
+  const options = {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(user),
+  };
+
+  return fetch(url, options).then(res => {
+    if (res.ok) {
+      return res.json();
+    } else {
+      throw new Error('Server Error');
+    }
+  });
+}
+export function deleteUser(id) {
+  const END_POINT = `/users/${id}`;
+  const url = BASE_URL + END_POINT;
+
+  const options = {
+    method: 'DELETE',
+  };
+
+  return fetch(url, options).then(res => {
+    if (res.ok) {
+      return res.json();
+    } else {
+      throw new Error('Server Error');
+    }
+  });
 }
